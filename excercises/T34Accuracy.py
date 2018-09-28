7723db07cbbae41c522d0e1e2dbd18c63e41dff6
Zoltán Tudlik <zoltan.tudlik@gmail.com>
2018-09-28 13:43:27 +0300

    calculate accuracy

--- excercises/T34Accuracy.py
+++ excercises/T34Accuracy.py
@@ -1,6 +1,23 @@
-def calculate_accuracy(y,y_hat):
+import numpy as np
+import pandas as pd
+
+from excercises.T32LogisticRegression import logisticRegression_func, predict_output
+
+
+def calculate_accuracy(y, y_hat):
     ### STUDENT TASK ###
     # YOUR CODE HERE
-    raise NotImplementedError()
+    N = len(y)
+    accuracy = np.sum(y == y_hat) / N
     return accuracy
-print ('Accuracy of the result is: %f%%' % calculate_accuracy(y,y_hat))
+
+
+X = np.array(pd.read_csv("feature_map.csv"))
+y = np.array(pd.read_csv("labels.csv"))
+step_size = 1e-5
+num_iter = 3000
+
+e_list, w_opt = logisticRegression_func(X, y, step_size, num_iter)
+y_hat = predict_output(X, w_opt).reshape((len(X), 1))
+
+print('Accuracy of the result is: %f%%' % calculate_accuracy(y, y_hat))
